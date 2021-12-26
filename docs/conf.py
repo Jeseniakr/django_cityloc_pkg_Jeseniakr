0836f96a15c87608ecdb6e59334f79f261b3f08a
Jesenia Rodriguez <jkr773@gmail.com>
2021-12-26 12:54:42 -0500

    publish package

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'django_cityloc_pkg'
+project = 'django_cityloc_pkg_Jeseniakr'
 copyright = '2021, Author Name'
 author = 'Author Name'
 
